0420327539af96327852c3878bed793efc259f8f
jothbc <jothbc@gmail.com>
2020-04-06 16:32:24 -0300

    Criando parte de login de Cliente

---
+++ Conexao/JDBC.php
@@ -0,0 +1,45 @@
+<?php
+class Conexao
+{
+    private $host = '192.168.0.158';
+    private $dbname = 'php_pedidos';
+    private $user = 'root';
+    private $pass = '';
+
+    public function conectar()
+    {
+        try {
+            $conexao = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname",
+                "$this->user",
+                "$this->pass"
+            );
+            return $conexao;
+        } catch (PDOException $e) {
+            echo '<p>' . $e->getMessage() . '</p>';
+        }
+    }
+}
+class ConexaoFirebird
+{
+    private $host = 'firebird:dbname=192.168.0.158:C:/IBS/Sisfatura/SISFATURA.FDB; charset = utf-8;';
+    private $password = 'masterkey';
+    private $username = 'SYSDBA';
+
+    public function conectar()
+    {
+        try {
+            $firebird = new PDO(
+                $this->host,
+                $this->username,
+                $this->password,
+                [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+            );
+            return $firebird;
+        } catch (PDOException $e) {
+            echo '<p>' . $e->getMessage() . '</p>';
+        }
+    }
+}
+
+?>
---
+++ Login/login.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once "../../../app_cotacao/Cliente/Cliente.model.php";
+require_once "../../../app_cotacao/Cliente/ClienteService.php";
+
+if (!isset($_POST['email']) && !isset($_POST['senha'])) {
+    session_destroy();
+    header("Location: ../index.php?erro=login");
+}
+if (isset($_POST['cliente']) && $_POST['cliente'] == 1) {
+    //cliente tentando logar
+    $cliente = new Cliente();
+    $cliente->__set('email', $_POST['email']) ;
+    $cliente->__set('pass', $_POST['senha']) ;
+
+    $conexao = new Conexao();
+    $service = new ClienteService($cliente,$conexao);
+    $novo = $service->authencitation();
+    if($novo){
+        //autenticou
+        $_SESSION['authenticate'] = 'yes';
+        $_SESSION['id'] = $novo['id'];
+        $_SESSION['company_name'] = $novo['company_name'];
+
+        header("Location: ../Cliente/index.php");
+    }else{
+        session_destroy();
+        header("Location: ../index.php?erro=usuario");
+    }
+} else if (isset($_POST['fornecedor']) && $_POST['fornecedor'] == 1) {
+    //fornecedor tentando logar
+}
